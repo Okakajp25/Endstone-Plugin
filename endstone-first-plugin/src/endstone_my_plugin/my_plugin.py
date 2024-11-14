ff2ccd23ff7b6127dec5d275633c8c9772fb0326
おかーかじぇーぴー <kaitosatou0025@gmail.com>
2024-11-14 09:14:36 +0000

    a

--- endstone-first-plugin/src/endstone_my_plugin/my_plugin.py
+++ endstone-first-plugin/src/endstone_my_plugin/my_plugin.py
@@ -1,6 +1,13 @@
 from endstone.plugin import Plugin
+from endstone.event import event_handler, PlayerJoinEvent
+from endstone import ColorFormat
 
 class MyPlugin(Plugin):
     api_version: "0.4"
     def on_load(self) -> None:
         self.logger.info("on_load")
+    def on_player_join(self, event: PlayerJoinEvent):
+        player = event.player
+        self.plugin.logger.info(
+            ColorFormat.YELLOW + f"{player.name}/[{player.address}:{player.port}] joined the game with UUID {player.unique_id}"
+        )
